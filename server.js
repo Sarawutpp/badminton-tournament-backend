d3e3679dd832a8f7dc9e2bb025c99ad1aa39641e
Sarawut <sarawut.playlamool@gmail.com>
2025-09-14 13:35:38 +0700

    sever1

--- server.js
+++ server.js
@@ -5,9 +5,11 @@ const app = require('./src/app');
 
 
 const PORT = process.env.PORT || 5000;
+const HOST = '0.0.0.0'; // <-- เพิ่มบรรทัดนี้: เพื่อให้ Server รับการเชื่อมต่อจากภายนอก
 const server = createServer(app);
 
 
-server.listen(PORT, () => {
-console.log(`API running on http://localhost:${PORT}`);
+// แก้ไขบรรทัดนี้: เพิ่ม HOST เข้าไป
+server.listen(PORT, HOST, () => {
+  console.log(`API running on http://${HOST}:${PORT}`);
 });// deploy-test Sat 09/06/2025 14:34:30.23
